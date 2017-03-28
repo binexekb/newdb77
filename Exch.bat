cfa97e387f2bf60538fd7c73d5bb7fc280db16aa
binex <ipspirid@gmail.com>
2017-03-28 09:24:09 +0500

    Очистка документов к выгрузке опционально по префиксу базы. Вывод текста лога - последние 50 строк.

--- Exch.bat
+++ Exch.bat
@@ -1,1 +1,2 @@
-"C:\Program Files (x86)\1Cv77\BIN\1cv7s.exe" CONFIG /DC:\Buhg\G\SQL_DEBUG /NExch /P123456789 /@c:\REPO\newdb77\instruct
+rem"C:\Program Files (x86)\1Cv77\BIN\1cv7s.exe" CONFIG /DC:\Buhg\G\SQL_DEBUG /NExch /P123456789 /@c:\REPO\newdb77\instruct
+"C:\Program Files (x86)\1Cv77\BIN\1cv7s.exe" CONFIG /DC:\Buhg\G\SQLDB /NExch /P123456789 /@c:\REPO\newdb77\instruct
